22e1048c3bacd6defa7d10fc1fc99d5453c87e0b
Joshua Ibbotson <josh@Joshuas-MacBook-Pro-2.local>
2022-05-08 19:47:41 +0100

    note can be added, but not edited or removed.

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-document.getElementById('submitFormBtn').addEventListener('click', revealNote());
 
+// reveals note creator form//
+const plus = document.getElementById('circle')
+plus.addEventListener("click", revealNoteCreator)
+
+function revealNoteCreator() {
+    document.getElementById("mainContainer").style.display = "flex";
+}
+
+// takes form values and puts it into localstorage//
 document.getElementById('description').addEventListener('input', function() {
+
     const time = new Date().toLocaleString();
     localStorage.setItem('Note', JSON.stringify({ title: title.value, description: description.value}));
     localStorage.setItem('Time', JSON.stringify({ date: time}));
-})
+});
 
-
+// creates note from local storage data and maker note creator hidden//
+document.getElementById('submitFormBtn').addEventListener('click', revealNote());
 
 function revealNote() {
-    let titleReveal = JSON.parse(localStorage.getItem('Note')).title;
+    let titleReveal = JSON.parse(localStorage.getItem('Note')).title[02];
     document.getElementById('titleProjection').textContent = titleReveal;
 
     let descriptionReveal = JSON.parse(localStorage.getItem('Note')).description;
@@ -17,7 +27,11 @@ function revealNote() {
     
     let timeReveal = JSON.parse(localStorage.getItem('Time')).date;
     document.getElementById('timeProjection').textContent = timeReveal;
+
+    document.getElementById("mainContainer").style.display = "none";
+
 }
+
 
 
 
